96930ae1b94b5bd1f31065350aeeab2900fb5db3
ccslakey <cromwellslakey@gmail.com>
2015-12-04 16:27:01 -0800

    change procfile?

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ var server = app.listen(3000, function() {
     var port = process.env.PORT || server.address().port;;
 
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('App listening at http://%s:%s', host, port);
 });
